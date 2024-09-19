090c3d9ddd2105c6560a4685f00f404caa27cf0a
Kiransai217 <kiransaikasani217@gmail.com>
2024-09-19 16:22:17 +0530

    Add files via upload

---
+++ squareroot.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    float a,squareroot;
+    printf("Enter the value:");
+    scanf("%f",&a);
+    squareroot= sqrt(a);
+    printf("Square Root=%f",squareroot);
+}
